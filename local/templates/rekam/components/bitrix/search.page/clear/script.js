a00f722c6629fd418bb556066ffa79acb312c28a
Andrey Abalakov <a.s.abalakov@yandex.ru>
2019-08-09 11:48:49 +0300

    Очищен файл скрипта. Пока в нем нет необходимости

--- local/templates/rekam/components/bitrix/search.page/clear/script.js
+++ local/templates/rekam/components/bitrix/search.page/clear/script.js
@@ -1,19 +1,2 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
 $(document).ready(function () {
-  var $sectionFilter = getUrlParameter('sectionFilter');
-  if ($sectionFilter == undefined || $sectionFilter == '') $("#sectionFilter").val('abcdefghijklmnopqrstuvwxyz');
-  else $("#sectionFilter").val(getUrlParameter('sectionFilter'));
 });
