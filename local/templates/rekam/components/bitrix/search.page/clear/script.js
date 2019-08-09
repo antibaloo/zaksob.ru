ec866b81deef8927beaf42f3a8ef97b0794e2141
Andrey Abalakov <a.s.abalakov@yandex.ru>
2019-08-09 09:30:48 +0300

    Добавлен механизм заполнения поля фильтрации по разделам по умолчанию

--- local/templates/rekam/components/bitrix/search.page/clear/script.js
+++ local/templates/rekam/components/bitrix/search.page/clear/script.js
@@ -1,3 +1,21 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
 $(document).ready(function () {
-  console.log("Works!!!")
+  console.log("Ready!!!");
+  var $sectionFilter = getUrlParameter('sectionFilter');
+  console.log($sectionFilter);
+  if ($sectionFilter == undefined || $sectionFilter == '') $("#sectionFilter").val('abcdefghijklmnopqrstuvwxyz');
+  else $("#sectionFilter").val(getUrlParameter('sectionFilter'));
 });
