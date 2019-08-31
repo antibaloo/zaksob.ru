76eeaad63dce3bf3c076b36d6b7b6a4b0e950243
Andrey Abalakov <a.s.abalakov@yandex.ru>
2019-08-31 10:15:11 +0300

    Исправлена ошибка: путь к файлу favicom.ico был указан относительно текущей папки, в результате он не отображался нигде, кроме корня сайта

--- local/templates/rekam/header.php
+++ local/templates/rekam/header.php
@@ -20,7 +20,7 @@ else
     </script>
     <? $APPLICATION->ShowHead(); ?>
     <title><? $APPLICATION->ShowTitle() ?></title>
-    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Vollkorn:700&amp;text=%D0%97%D0%B0%D0%BA%D0%BE%D0%BD%D0%B4%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%81%D0%A1%D0%B1%D0%9E%D1%80%D0%B8%D1%83%D0%B3%D0%B9%20&amp;subset=cyrillic" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
