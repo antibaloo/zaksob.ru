c2e49a8ad5e12e114c025b89106d4d9ff81738ae
Andrey Abalakov <a.s.abalakov@yandex.ru>
2020-12-16 12:37:45 +0300

    Исправлена ошибка вывода месяца в компоненте календаря

--- activity/zakonotvorcheskaya-deyatelnost/zakony-i-postanovleniya/index.php
+++ activity/zakonotvorcheskaya-deyatelnost/zakony-i-postanovleniya/index.php
@@ -1,11 +1,10 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Законы и постановления");
+CJSCore::Init(array("date"));
 ?>
 
-
 <?
-
 global $arFilter;
 $arFilter = [];
 if (!empty($_REQUEST['q'])){
